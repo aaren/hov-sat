c98dc266f1384525203e15dc5d4bc42a4b99b1b0
Aaron O'Leary <eeaol@leeds.ac.uk>
2012-11-01 17:28:17 +0000

    details

---
+++ hovsat.py
@@ -0,0 +1,18 @@
+# Read in image and fit to lat lon grid
+# Return 2d array for image pixel intensities
+# and 2d array of coords
+
+# With the array, select values along a particular transect.
+# may require interpolation to a higher resolution grid.
+# return a 1d array of intensities and a 1d array of coords
+
+# process multiple images in a sequence and stack 1d arrays
+# into a 2d array that has dimensions of space x time
+# returns a 2d array
+
+# plot a given 2d array as a hovmoller
+
+# bonus: intelligently interpolate the hovmoller such that
+# coherent wave structures are picked out. requires selection of
+# appropriate interpolation lines in image to create an
+# interpolation field.
